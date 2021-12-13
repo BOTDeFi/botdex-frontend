e93fdaf784afe00bc038d41f256ce55bcab03ba3
Gleb Chistyakov <gleb4ist@gmail.com>
2021-12-13 11:09:47 +0300

    fix test url

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -4,5 +4,5 @@ export const localApi = axios.create({
   baseURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/'
-      : 'https://rifenery-dao.herokuapp.com/', // TODO: remove this when `git rebase` or `git merge`
+      : 'https://strong-hands.herokuapp.com/', // TODO: remove this when `git rebase` or `git merge`
 });
