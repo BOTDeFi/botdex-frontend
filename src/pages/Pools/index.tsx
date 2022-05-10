3cbc5ea430dbdc1c67acc87e040ee3ab513d4ba3
Anton Polenyaka <antonpolenyaka@sitel-sa.com>
2022-05-10 22:27:20 +0200

    not infinite spiner in pools when wallet is not connected
    not infinite spiner in pools when wallet is not connected

--- src/pages/Pools/index.tsx
+++ src/pages/Pools/index.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect } from 'react';
-import { LoadingOutlined } from '@ant-design/icons';
-import { Spin } from 'antd';
-import { observer } from 'mobx-react-lite';
-
+import { ReactComponent as WalletImg } from '@/assets/img/icons/wallet.svg';
+import Button from '@/components/atoms/Button';
 import { CollectModal, StakeUnstakeModal } from '@/components/organisms';
+import { WalletModal } from '@/components/sections';
 import { PoolsPreview, StakeCard } from '@/components/sections/Pools';
 import useRefresh from '@/hooks/useRefresh';
+import { useWalletConnectorContext } from '@/services/MetamaskConnect';
 // import useRefresh from '@/hooks/useRefresh';
 import { useMst } from '@/store';
-
+import { addressWithDots } from '@/utils/formatters';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect, useState } from 'react';
 import './Pools.scss';
 
-const antIcon = <LoadingOutlined style={{ fontSize: 96 }} spin />;
-
 const Pools: React.FC = observer(() => {
+  const { connect } = useWalletConnectorContext();
+  const [isWalletModalVisible, setWalletModalVisible] = useState(false);
+
   const { stakes: stakeStore, user } = useMst();
   const { fastRefresh } = useRefresh();
   useEffect(() => {
@@ -35,8 +37,49 @@ const Pools: React.FC = observer(() => {
                 ))}
               </div>
             ) : (
-              <Spin className="spinner" size="large" indicator={antIcon} />
-            )}
+              <div className="spinner">
+                <h2 className="ShadowTitle_title__1a_EI ShadowTitle_white__1EFkK">
+                  <span>No possibility to make stake</span>
+                  <span>No possibility to make stake</span>
+                </h2>
+                <div className="subTitleDescription">
+                  No available slots to stake. Try it late!
+                </div>
+                <div className="breakFlex" />
+                <div className="divCenter">
+                  {!user.address ? (
+                    <Button
+                      className="connect btn-hover-down"
+                      colorScheme="blue"
+                      size="sm"
+                      onClick={connect}
+                    >
+                      <WalletImg />
+                      <span className="text-500">Connect Wallet</span>
+                    </Button>
+                  ) : (
+                    <Button
+                      className="connect btn-hover-down"
+                      colorScheme="blue"
+                      size="sm"
+                      onClick={() => setWalletModalVisible(true)}
+                    >
+                      <WalletImg />
+                      <span className="text-bold text-address">{addressWithDots(user.address)}</span>
+                    </Button>
+                  )}
+                  {user.address ? (
+                    <WalletModal
+                      isVisible={isWalletModalVisible}
+                      handleClose={() => setWalletModalVisible(false)}
+                    />
+                  ) : (
+                    ''
+                  )}
+                </div>
+              </div>
+            )
+            }
           </div>
         </div>
       </main>
