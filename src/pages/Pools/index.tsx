97a7d4ce0e43ea095a235d2282b9a9c8c53b0025
Anton Polenyaka <anton@antonp.com>
2022-05-19 18:05:59 +0200

    Change pool & farms connect wallet

--- src/pages/Pools/index.tsx
+++ src/pages/Pools/index.tsx
@@ -1,4 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { observer } from 'mobx-react-lite';
+
 import { ReactComponent as WalletImg } from '@/assets/img/icons/wallet.svg';
+import { ShadowTitle } from '@/components/atoms';
 import Button from '@/components/atoms/Button';
 import { CollectModal, StakeUnstakeModal } from '@/components/organisms';
 import { WalletModal } from '@/components/sections';
@@ -8,8 +12,7 @@ import { useWalletConnectorContext } from '@/services/MetamaskConnect';
 // import useRefresh from '@/hooks/useRefresh';
 import { useMst } from '@/store';
 import { addressWithDots } from '@/utils/formatters';
-import { observer } from 'mobx-react-lite';
-import React, { useEffect, useState } from 'react';
+
 import './Pools.scss';
 
 const Pools: React.FC = observer(() => {
@@ -24,62 +27,72 @@ const Pools: React.FC = observer(() => {
     }
   }, [stakeStore, user.address, fastRefresh]);
 
+  const stakeCards = (
+    <div className="pools__content-card-view">
+      {stakeStore.data.map((stake: any) => (
+        <StakeCard key={stake.id} stake={stake} />
+      ))}
+    </div>
+  );
+
+  const noStakingPools = (
+    <div className="spinner">
+      {!user.address ? (
+        <>
+          <ShadowTitle type="h2">Wallet not connected</ShadowTitle>
+          <div className="subTitleDescription">
+            For staking, you need to connect a wallet compatible with web3 (for example MetaMask)
+            used Binance Smart Chain (BSC) network
+          </div>
+          <div className="breakFlex" />
+          <div>
+            <Button
+              className="connect btn-hover-down"
+              colorScheme="blue"
+              size="sm"
+              onClick={connect}
+            >
+              <WalletImg />
+              <span className="text-500">Connect Wallet</span>
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <ShadowTitle type="h2">No possibility to make stake</ShadowTitle>
+          <div className="subTitleDescription">No available slots to stake. Try it late</div>
+          <div className="breakFlex" />
+          <div>
+            <Button
+              className="connect btn-hover-down"
+              colorScheme="blue"
+              size="sm"
+              onClick={() => setWalletModalVisible(true)}
+            >
+              <WalletImg />
+              <span className="text-bold text-address">{addressWithDots(user.address)}</span>
+            </Button>
+          </div>
+        </>
+      )}
+      {user.address ? (
+        <WalletModal
+          isVisible={isWalletModalVisible}
+          handleClose={() => setWalletModalVisible(false)}
+        />
+      ) : (
+        ''
+      )}
+    </div>
+  );
+
   return (
     <>
       <main className="pools">
         <div className="row">
           <PoolsPreview />
           <div className="pools__content">
-            {stakeStore.data.length > 0 ? (
-              <div className="pools__content-card-view">
-                {stakeStore.data.map((stake: any) => (
-                  <StakeCard key={stake.id} stake={stake} />
-                ))}
-              </div>
-            ) : (
-              <div className="spinner">
-                <h2 className="ShadowTitle_title__1a_EI ShadowTitle_white__1EFkK">
-                  <span>No possibility to make stake</span>
-                  <span>No possibility to make stake</span>
-                </h2>
-                <div className="subTitleDescription">
-                  No available slots to stake. Try it late!
-                </div>
-                <div className="breakFlex" />
-                <div className="divCenter">
-                  {!user.address ? (
-                    <Button
-                      className="connect btn-hover-down"
-                      colorScheme="blue"
-                      size="sm"
-                      onClick={connect}
-                    >
-                      <WalletImg />
-                      <span className="text-500">Connect Wallet</span>
-                    </Button>
-                  ) : (
-                    <Button
-                      className="connect btn-hover-down"
-                      colorScheme="blue"
-                      size="sm"
-                      onClick={() => setWalletModalVisible(true)}
-                    >
-                      <WalletImg />
-                      <span className="text-bold text-address">{addressWithDots(user.address)}</span>
-                    </Button>
-                  )}
-                  {user.address ? (
-                    <WalletModal
-                      isVisible={isWalletModalVisible}
-                      handleClose={() => setWalletModalVisible(false)}
-                    />
-                  ) : (
-                    ''
-                  )}
-                </div>
-              </div>
-            )
-            }
+            {stakeStore.data.length > 0 ? stakeCards : noStakingPools}
           </div>
         </div>
       </main>
